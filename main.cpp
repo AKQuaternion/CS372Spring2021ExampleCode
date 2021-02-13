9638cc993cb0470bdd435a2aeda29e75c322b3f8
Chris Hartman <chris.m.hartman@gmail.com>
2021-02-12 16:07:17 -0900

    C++11 async (and futures) (and locks) example

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <chrono>
 #include <concepts>
 #include <execution>
-#include <numeric>
 #include <iostream>
+#include <numeric>
 #include <random>
 #include <vector>
 using std::cout;
@@ -12,15 +12,14 @@ using std::vector;
 #include <string>
 #include <tuple>
 using std::tuple;
-#include <map>
-void modernCppExamples();
 #include <functional>
+#include <map>
 
 double add1MRandomDoubles() {
    const vector<double> doublesToAdd = []() {
-      std::random_device r; //truly random on many/most platforms (but low entropy, call only once)
-      std::default_random_engine e1(r()); //seed the random engine
-      std::uniform_real_distribution<double> uniform_dist(0.0, 10.0); //create a distribution
+      std::random_device r;                                          //truly random on many/most platforms (but low entropy, call only once)
+      std::default_random_engine e1(r());                            //seed the random engine
+      std::uniform_real_distribution<double> uniform_dist(0.0, 10.0);//create a distribution
 
       vector<double> v;
       const int SIZE = 1'000'000;
@@ -30,63 +29,74 @@ double add1MRandomDoubles() {
       return v;
    }();
    //   return std::reduce(std::execution::par, doublesToAdd.begin(), doublesToAdd.end())
-   return std::accumulate(doublesToAdd.begin(),doublesToAdd.end(),0.0);
+   return std::accumulate(doublesToAdd.begin(), doublesToAdd.end(), 0.0);
 };
 
 struct Foo {
    int x;
-   double d=13.0;
+   double d = 13.0;
    char c;
-   int operator()(double r) {return r*d;}
+   int operator()(double r) { return r * d; }
 };
 
-bool operator<(const Foo &lhs,const Foo &rhs) {
-return std::forward_as_tuple(lhs.x,lhs.d,lhs.c) < std::forward_as_tuple(rhs.x,rhs.d,rhs.c);
+bool operator<(const Foo &lhs, const Foo &rhs) {
+   return std::forward_as_tuple(lhs.x, lhs.d, lhs.c) < std::forward_as_tuple(rhs.x, rhs.d, rhs.c);
 }
 
-tuple<int,double,char> f{7,2.4,'x'};
-tuple<int,double,char> g{7,2.3,'r'};
-auto h=std::make_tuple(14,13.13,'m');
+tuple<int, double, char> f{7, 2.4, 'x'};
+tuple<int, double, char> g{7, 2.3, 'r'};
+auto h = std::make_tuple(14, 13.13, 'm');
 
 int foo(double d) {
-   return d*d;
+   return d * d;
 }
 
+void async_main();
 
+void foo(std::string_view s) {
+   cout << s;
+}
 
 int main() {
-//vector<int> v{2,4,6,8,9,10};
+//   vector v{2,4,6,8,9,10};
+//   std::pair p{2,'c'};
+//   std::string s("world");
+//   foo(s);
 //
-//if (std::all_of(v.begin(),v.end(),[](auto i){return i%2==0;}))
-//   cout << "Everything in v is even.\n";
-//  int i=13;
-//  auto multiplyByI = [=](int x) {return x*i;};
-//  i=2;
-//  cout << multiplyByI(5) << endl;
-
-//std::function<int (double)> myFunc = foo;
-//myFunc = [](double d) {return int(d*d*d);};
-//   Foo f;
-//myFunc = f;
+//   if(auto c=getchar(); c!='\0') {
 //
-//cout << myFunc(13.3) << endl;
+//   }
+   //
+   //if (std::all_of(v.begin(),v.end(),[](auto i){return i%2==0;}))
+   //   cout << "Everything in v is even.\n";
+   //  int i=13;
+   //  auto multiplyByI = [=](int x) {return x*i;};
+   //  i=2;
+   //  cout << multiplyByI(5) << endl;
+
+   //std::function<int (double)> myFunc = foo;
+   //myFunc = [](double d) {return int(d*d*d);};
+   //   Foo f;
+   //myFunc = f;
+   //
+   //cout << myFunc(13.3) << endl;
 
 
+   //  vector<int> v(100);
+   //  std::iota(v.begin(), v.end(), 1);
+   //  auto fac = std::accumulate(
+   //      v.begin() + 1, v.end(), std::to_string(v.front()),
+   //      [](auto i, auto j) { return i + ", " + std::to_string(j); });
+   //  cout << fac << fac << endl;
 
-//  vector<int> v(100);
-//  std::iota(v.begin(), v.end(), 1);
-//  auto fac = std::accumulate(
-//      v.begin() + 1, v.end(), std::to_string(v.front()),
-//      [](auto i, auto j) { return i + ", " + std::to_string(j); });
-//  cout << fac << fac << endl;
-
-//   auto x{4};
-//   auto s = std::string{"hello"};
-//   std::string f{};
-//
-   auto start = std::chrono::steady_clock::now();
-   cout << "Sum was " << add1MRandomDoubles() << endl;
-   auto end = std::chrono::steady_clock::now();
-   std::chrono::duration<double> elapsed_seconds  = end - start;
-   std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+   //   auto x{4};
+   //   auto s = std::string{"hello"};
+   //   std::string f{};
+   //
+   //   auto start = std::chrono::steady_clock::now();
+   //   cout << "Sum was " << add1MRandomDoubles() << endl;
+   //   auto end = std::chrono::steady_clock::now();
+   //   std::chrono::duration<double> elapsed_seconds  = end - start;
+   //   std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+   async_main();
 }
--- modernCPPexamples.cpp
+++ modernCPPexamples.cpp
@@ -23,11 +23,12 @@ using std::mutex;
 using std::unique_lock;
 #include <algorithm>
 using std::all_of;
+using std::for_each;
 #include <array>
+#include <functional>
 #include <numeric>
 #include <tuple>
 #include <utility>
-#include <functional>
 
 auto magicNumber(string_view word) {
    unsigned long answer = 0;
@@ -36,21 +37,7 @@ auto magicNumber(string_view word) {
    return answer;
 }
 
-void checkAWord(string_view word, mutex &m) {
-   if (magicNumber(word) % 1000 == 0) {
-//      unique_lock lock(m);
-      cout << word << " " << magicNumber(word) << endl;
-   }
-}
-
-template <typename Iter> void checkSomeWords(Iter first, Iter last, mutex &m) {
-   for (Iter i = first; i != last; ++i)
-      checkAWord(*i,m);
-}
-
 void async_main() {
-
-   mutex m;
    auto startTime = std::chrono::steady_clock::now();
 
    ifstream ifile("/usr/share/dict/words");
@@ -59,62 +46,54 @@ void async_main() {
    while (ifile >> word)
       dictionary.push_back(word);
 
-   vector<std::future<void>> results;
-   const int numThreads = 10;
+   vector<std::future<int>> results;
+   const int numThreads = 80;
+   mutex m;
    for (int i = 0; i < numThreads; ++i) {
       auto numWords = dictionary.size();
       auto start = int(i / double(numThreads) * numWords) + begin(dictionary);
       auto finish = int((i + 1) / double(numThreads) * numWords) +
                     begin(dictionary);
 
-      results.push_back(std::async(std::launch::async,
-                                   checkSomeWords<vector<string>::const_iterator>,
-                                   start, finish, std::ref(m)));
+      auto processSome = [start,finish,&m]() {
+         int numMagicWords = 0;
+         std::for_each(start, finish, [&numMagicWords,&m](const auto &word) {
+            if (magicNumber(word) % 1000 == 0) {
+               ++numMagicWords;
+               unique_lock lock(m);
+               if(magicNumber(word) % 3 == 0)
+                  return;
+               cout << word << " " << magicNumber(word) << endl;
+            }
+         });
+         return numMagicWords;
+      };
+      results.push_back(std::async(std::launch::async, processSome));
    }
-//
-   for (auto &f : results)
-      f.get();
 
-//    std::for_each(//std::execution::par,
-//    dictionary.begin(),dictionary.end(),checkAWord);
+   int sum=0;
+   for (auto &f : results)
+      sum += f.get();
+   cout << sum << " magic words.\n";
    auto endTime = std::chrono::steady_clock::now();
    auto diff = endTime - startTime;
    cout << std::chrono::duration<double>(diff).count() << "s" << endl;
 }
 
-class Foo {
-public:
-   explicit Foo(int x) : _x(x)
-   {}
-
-   [[nodiscard]] std::string operator()() {
-      return "This is Foo::operator()";
-   }
-   [[nodiscard]] auto operator()(int i) {
-      return i*_x;
-   }
-private:
-   int _x;
-};
-
-int bar(int) {
-   return 13;
-}
-
 void modernCppExamples() {
-   Foo f{5};
-
-   cout << f(3) << endl;
-
-   std::function<int(int)> g = f;
-   g = bar;
-   g = [](int x) {return x*x;};
-
-   auto isOdd = [](int x) {return x%2==1;};
-
-   if (isOdd(5)) cout << "5 is odd";
-
-   using namespace std::string_literals;
+   //   Foo f{5};
+   //
+   //   cout << f(3) << endl;
+   //
+   //   std::function<int(int)> g = f;
+   //   g = bar;
+   //   g = [](int x) {return x*x;};
+   //
+   //   auto isOdd = [](int x) {return x%2==1;};
+   //
+   //   if (isOdd(5)) cout << "5 is odd";
+   //
+   //   using namespace std::string_literals;
 
 
    //  auto [inserted,iterToIt] = rp();
